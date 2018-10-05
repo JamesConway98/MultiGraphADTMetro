6439df106969b43b05fc7cfc77aa0475c37c61b8
James Conway CS2016 <james.conway.2016@uni.strath.ac.uk>
2018-10-05 18:36:15 +0100

    User Input Now accepted

--- src/BostonMetro.java
+++ src/BostonMetro.java
@@ -1,5 +1,4 @@
 import java.io.IOException;
-<<<<<<< HEAD
 import java.util.Scanner;
 
 public class BostonMetro
@@ -13,22 +12,6 @@ public class BostonMetro
 	//Multigraph graph = new Multigraph();
 	//MetroMapParser parser = new MetroMapParser("file.ext");
 	
-=======
-import java.util.ArrayList;
-import java.util.HashMap;
-
-public class BostonMetro
-{
-	private ArrayList<ArrayList<String>> nodes;
-    private String fileLocation;
-    HashMap<Integer, String> orange, blue, green, red;
-
-    public static void main(String[] args)
-    {
-
-    }
-
->>>>>>> b96dea26e38f72302391a58c7e65a20cab04a229
 	/*
 	parser.parse(); // creates HashMaps stations, greenLine, redLine ...
 	stations.forEach((id, station) -> {
@@ -40,35 +23,8 @@ public class BostonMetro
         ...
         something like that :)
 	*/
-    public BostonMetro() throws BadFileException {
-    	fileLocation = "H:/cis/windows/workspace/boston_metro/src/Stations.txt";
-    	orange = new HashMap<Integer, String>(32);
-    	blue  = new HashMap<Integer, String>(32);
-    	green  = new HashMap<Integer, String>(32);
-    	red  = new HashMap<Integer, String>(32);
-    	
-    	
-    	try {
-			read();
-		} catch (BadFileException e) {
-			// TODO Auto-generated catch block
-			throw new BadFileException("Incorrect File Source");
-		}
-    	lineMaps();
     }
     
-    private void read() throws BadFileException{
-    	try {
-			MetroMapParser reader = new MetroMapParser(fileLocation);
-			nodes = reader.generateGraphFromFile();
-		} catch (IOException | BadFileException e) {
-			throw new BadFileException("Incorrect File Source");
-			
-		}
-    	//System.out.print(nodes);  	
-    }
-    
-<<<<<<< HEAD
     
     public String findPath() throws NoSuchNodeException {
     	
@@ -139,37 +95,4 @@ public class BostonMetro
     	
     }
 
-=======
-    /* for each colour
-     * attach hashset with <stationID,stationLabel>
-     */
-    private void lineMaps(){
-    	// Orange!
-    	for(int i = 0; i < nodes.size(); i++){
-    			if(nodes.get(i).contains("Orange") && Integer.parseInt(nodes.get(i).get(0)) == i + 1){
-    				orange.put(i + 1, nodes.get(i).get(1));
-    				//System.out.print(nodes.get(i).get(1) + " was successfully added to hashmap orange," + " size: " + orange.size() + "\n");
-    		}
-    	}//blue!
-    	for(int i = 0; i < nodes.size(); i++){
-			if(nodes.get(i).contains("Blue") && Integer.parseInt(nodes.get(i).get(0)) == i + 1){
-				blue.put(i + 1, nodes.get(i).get(1));
-				System.out.print(nodes.get(i).get(1) + " was successfully added to hashmap blue," + " size: " + blue.size() + "\n");
-			}
-		}/*
-		//green!!	
-    	for(int i = 0; i < nodes.size(); i++){
-			if(nodes.get(i).contains("green") && Integer.parseInt(nodes.get(i).get(0)) == i + 1){
-				green.put(i + 1, nodes.get(i).get(1));
-			}
-		}*/
-    	/*red!!
-    	for(int i = 0; i < nodes.size(); i++){
-			if(nodes.get(i).contains("Red") && Integer.parseInt(nodes.get(i).get(0)) == i + 1){
-				red.put(i + 1, nodes.get(i).get(1));
-			}
-		}*/
-	}
-    
->>>>>>> b96dea26e38f72302391a58c7e65a20cab04a229
 }
