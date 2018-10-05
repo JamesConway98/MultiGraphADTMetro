b96dea26e38f72302391a58c7e65a20cab04a229
Jack Bowden SE2016 <jack.bowden.2016@uni.strath.ac.uk>
2018-10-05 17:42:57 +0100

    parser makes hashmap of red, blue, still to do green, red

--- src/BostonMetro.java
+++ src/BostonMetro.java
@@ -1,7 +1,18 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class BostonMetro
 {
-    public BostonMetro()
+	private ArrayList<ArrayList<String>> nodes;
+    private String fileLocation;
+    HashMap<Integer, String> orange, blue, green, red;
+
+    public static void main(String[] args)
     {
+
+    }
+
 	/*
 	IGraph graph = new Multigraph();
 	MetroParser parser = new MetroParser("file.ext");
@@ -15,5 +26,63 @@ public class BostonMetro
         ...
         something like that :)
 	*/
+    public BostonMetro() throws BadFileException {
+    	fileLocation = "H:/cis/windows/workspace/boston_metro/src/Stations.txt";
+    	orange = new HashMap<Integer, String>(32);
+    	blue  = new HashMap<Integer, String>(32);
+    	green  = new HashMap<Integer, String>(32);
+    	red  = new HashMap<Integer, String>(32);
+    	
+    	
+    	try {
+			read();
+		} catch (BadFileException e) {
+			// TODO Auto-generated catch block
+			throw new BadFileException("Incorrect File Source");
+		}
+    	lineMaps();
     }
+    
+    private void read() throws BadFileException{
+    	try {
+			MetroMapParser reader = new MetroMapParser(fileLocation);
+			nodes = reader.generateGraphFromFile();
+		} catch (IOException | BadFileException e) {
+			throw new BadFileException("Incorrect File Source");
+			
+		}
+    	//System.out.print(nodes);  	
+    }
+    
+    /* for each colour
+     * attach hashset with <stationID,stationLabel>
+     */
+    private void lineMaps(){
+    	// Orange!
+    	for(int i = 0; i < nodes.size(); i++){
+    			if(nodes.get(i).contains("Orange") && Integer.parseInt(nodes.get(i).get(0)) == i + 1){
+    				orange.put(i + 1, nodes.get(i).get(1));
+    				//System.out.print(nodes.get(i).get(1) + " was successfully added to hashmap orange," + " size: " + orange.size() + "\n");
+    		}
+    	}//blue!
+    	for(int i = 0; i < nodes.size(); i++){
+			if(nodes.get(i).contains("Blue") && Integer.parseInt(nodes.get(i).get(0)) == i + 1){
+				blue.put(i + 1, nodes.get(i).get(1));
+				System.out.print(nodes.get(i).get(1) + " was successfully added to hashmap blue," + " size: " + blue.size() + "\n");
+			}
+		}/*
+		//green!!	
+    	for(int i = 0; i < nodes.size(); i++){
+			if(nodes.get(i).contains("green") && Integer.parseInt(nodes.get(i).get(0)) == i + 1){
+				green.put(i + 1, nodes.get(i).get(1));
+			}
+		}*/
+    	/*red!!
+    	for(int i = 0; i < nodes.size(); i++){
+			if(nodes.get(i).contains("Red") && Integer.parseInt(nodes.get(i).get(0)) == i + 1){
+				red.put(i + 1, nodes.get(i).get(1));
+			}
+		}*/
+	}
+    
 }
