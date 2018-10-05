4a9c7da41dd05cb603b68bec7d5f4e658dfb6676
James Conway CS2016 <james.conway.2016@uni.strath.ac.uk>
2018-10-05 18:58:57 +0100

    User Input Now accepted

--- src/BostonMetro.java
+++ src/BostonMetro.java
@@ -1,28 +1,36 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class BostonMetro
 {
 	
+	private ArrayList<ArrayList<String>> nodes;
+    private String fileLocation;
+    HashMap<Integer, String> orange, blue, green, red;
+
+	
 	private Multigraph graph;
 	
-    public BostonMetro() throws IOException
+    public BostonMetro() throws IOException, BadFileException
     {
 	
-	//Multigraph graph = new Multigraph();
-	//MetroMapParser parser = new MetroMapParser("file.ext");
-	
-	/*
-	parser.parse(); // creates HashMaps stations, greenLine, redLine ...
-	stations.forEach((id, station) -> {
-            graph.addNode(id, station);
-        });
-        greenLine.forEach((id1, id2) -> {
-            graph.addEdge(graph.getNodeById(id1), graph.getNodeById(id2), "green");
-        });
-        ...
-        something like that :)
-	*/
+    	fileLocation = "src/Stations.txt";
+    	orange = new HashMap<Integer, String>(32);
+    	blue  = new HashMap<Integer, String>(32);
+    	green  = new HashMap<Integer, String>(64);
+    	red  = new HashMap<Integer, String>(32);
+    	
+    	
+    	try {
+			read();
+		} catch (BadFileException e) {
+			// TODO Auto-generated catch block
+			throw new BadFileException("Incorrect File Source");
+		}
+    	lineMaps();
+
     }
     
     
@@ -94,5 +102,52 @@ public class BostonMetro
     	return null;
     	
     }
+    
+    
+    private void lineMaps(){
+    	// Orange!
+    	for(int i = 0; i < nodes.size(); i++){
+    			if(nodes.get(i).contains("Orange") && Integer.parseInt(nodes.get(i).get(0)) == i + 1){
+    				orange.put(i + 1, nodes.get(i).get(1));
+    				//System.out.print(nodes.get(i).get(1) + " was successfully added to hashmap orange," + " size: " + orange.size() + "\n");
+    		}
+    	}//blue!
+    	for(int i = 0; i < nodes.size(); i++){
+			if(nodes.get(i).contains("Blue") && Integer.parseInt(nodes.get(i).get(0)) == i + 1){
+				blue.put(i + 1, nodes.get(i).get(1));
+			}
+		}
+		//green!!	
+    	for(int i = 0; i < nodes.size(); i++){
+			if((nodes.get(i).contains("Green") 
+				|| nodes.get(i).contains("GreenB") 
+				|| nodes.get(i).contains("GreenC") 
+				|| nodes.get(i).contains("GreenD") 
+				|| nodes.get(i).contains("GreenE")) && Integer.parseInt(nodes.get(i).get(0)) == i + 1){
+				green.put(i + 1, nodes.get(i).get(1));	
+			}
+		}
+    	//red!!
+    	for(int i = 0; i < nodes.size(); i++){
+			if((nodes.get(i).contains("Red") 
+				|| nodes.get(i).contains("RedB")
+				|| nodes.get(i).contains("Mattapan")) && Integer.parseInt(nodes.get(i).get(0)) == i + 1){
+				red.put(i + 1, nodes.get(i).get(1));
+			}
+		}
+	}
+    
+    private void read() throws BadFileException{
+    	try {
+			MetroMapParser reader = new MetroMapParser(fileLocation);
+			nodes = reader.generateGraphFromFile();
+		} catch (IOException | BadFileException e) {
+			throw new BadFileException("Incorrect File Source");
+			
+		}
+    	//System.out.print(nodes);  	
+    }
+
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 
 public class Main
 {
-    public static void main(String[] args) throws NoSuchNodeException, IOException
+    public static void main(String[] args) throws NoSuchNodeException, IOException, BadFileException
     {
 		BostonMetro bm = new BostonMetro();
 		
